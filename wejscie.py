4afd3f3ee2b6b0e594912a17a45a96380f1a37e2
1C <WITSTOC32@edu.gdansk.pl>
2025-10-07 14:35:45 +0200

    lekcja o wyjsciu

---
+++ wejscie.py
@@ -0,0 +1,18 @@
+ #1. Wprowadzanie danych przez użytkownika do programu
+#1) utworzenie zmiennej
+#2) na ekranie pojawia sie komunikat z inputa
+#3) program sie zatrzymuje i czeka na podanie danych i zatwierdzenie tego ENTEREM
+#4) po wpisaniu i zatwierdzeniu ENTEREM to co wpiszemy trafia jako string do zmiennej
+#5) i program wykonuje sie dalej
+imie = input('podaj swoje imie')
+
+liczba = int(input('Podaj liczbe'))
+print(type(liczba))
+
+liczba_z_przecinkiem = float(input('Podaj liczbe z przecinkiem'))
+print(type(liczba_z_przecinkiem))
+
+cos = list(input('podaj coś'))
+print(cos)
+print(type(cos))
+
