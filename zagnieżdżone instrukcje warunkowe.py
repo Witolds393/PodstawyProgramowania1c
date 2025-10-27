4ce461720be8ad7f66b0872a6ac41b53c6d2c9af
Witek <you@test.com>
2025-10-27 18:50:07 +0100

    zagniedone instrukcje warunkowe.py

--- zagnieżdżone instrukcje warunkowe.py
+++ zagnieżdżone instrukcje warunkowe.py
@@ -2,7 +2,7 @@ a = float(input('Podaj liczbe rzeczywistą a różną od 0'))
 b = float(input('Podaj liczbe rzeczywistą b '))
 c = float(input('Podaj liczbe rzeczywistą c'))
 
-if b == 0 and c == 0 :
+if b == 0 and c == 0:
     print('Równanie = 0')
 if b == 0 and c != 0:
    if - c / a > 0:
@@ -12,3 +12,17 @@ if b == 0 and c != 0:
    if - c / a < 0:
        print('Nie ma rozwiazań')
 if c == 0 and b != 0:
+    x1 = 0
+    x2 = - (b / a)
+    print(f'x1 == {x1} v x2 == {x2}')
+if b != 0 and c != 0:
+    delta = b ** 2 - 4 * a * c
+    if delta > 0:
+        x1 = (-b + delta ** 0.5) / (2 * a)
+        x2 = (-b - delta ** 0.5) / (2 * a)
+        print(f'x1 == {x1} v x2 == {x2}')
+    if delta == 0:
+        x0 = -b / (2 * a)
+        print('x1 == x2 = {}'.format(x0))
+    if delta < 0:
+        print('nie ma rozwiązań')
