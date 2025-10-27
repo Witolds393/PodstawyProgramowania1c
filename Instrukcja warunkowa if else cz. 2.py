114debf99c027821f3175e503e827b705a48b5c4
Witek <you@test.com>
2025-10-27 17:54:17 +0100

    Instrukcja warunkowa if else cz. 2.py

---
+++ Instrukcja warunkowa if else cz. 2.py
@@ -0,0 +1,7 @@
+wynik_z_testu_klasyfikacyjnego = float(input('wynik_z_testu_klasyfikacyjnego'))
+Ocena_z_swiadectwa = int(input('ocena z świadectwa'))
+
+if wynik_z_testu_klasyfikacyjnego>= 90 or Ocena_z_swiadectwa>= 5 :
+    print('dostales się do grupy zaawansowanej')
+elif wynik_z_testu_klasyfikacyjnego< 90 or Ocena_z_swiadectwa< 5 :
+    print('grupa Podstawowa')
