c71575d516433bfbd3dc4cc24356f2a172eb776f
1C <WITSTOC32@edu.gdansk.pl>
2025-10-28 14:02:50 +0100

    zagniezdzone instrukcje warunkowe

--- zagnieżdżone instrukcje warunkowe.py
+++ zagnieżdżone instrukcje warunkowe.py
@@ -8,19 +8,19 @@ if b == 0 and c != 0:
    if - c / a > 0:
     x1 = (-c / a) ** 0.5
     x2 = -(-c / a) ** 0.5
-    print(f'x1 == {x1} v x2 == {x2}')
+    print(f'x1 = {x1} v x2 = {x2}')
    if - c / a < 0:
        print('Nie ma rozwiazań')
 if c == 0 and b != 0:
     x1 = 0
     x2 = - (b / a)
-    print(f'x1 == {x1} v x2 == {x2}')
+    print(f'x1 = {x1} v x2 = {x2}')
 if b != 0 and c != 0:
     delta = b ** 2 - 4 * a * c
     if delta > 0:
         x1 = (-b + delta ** 0.5) / (2 * a)
         x2 = (-b - delta ** 0.5) / (2 * a)
-        print(f'x1 == {x1} v x2 == {x2}')
+        print(f'x1 = {x1} v x2 = {x2}')
     if delta == 0:
         x0 = -b / (2 * a)
         print('x1 == x2 = {}'.format(x0))
