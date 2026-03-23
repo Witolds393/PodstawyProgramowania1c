301ce894a30c101d2e5abdba1adfe4636fc6e4e7
1C <WITSTOC32@edu.gdansk.pl>
2026-03-23 11:22:21 +0100

    wczytywanie plików danych

--- wczytywanie_danych_z_pliku.py
+++ wczytywanie_danych_z_pliku.py
@@ -1,3 +1,38 @@
-plik = open('plik')
+'''plik = open('plik')
 dane = plik.read()
-print(dane)
+print(dane)'''
+
+'''dane = open('plik2')
+plik2 = dane.readlines()
+print(plik2)
+
+for i in range(len(plik2)):
+    plik2[i] = int(plik2[i])
+print(plik2)'''
+
+'''dane3 = open('plik3')
+plik3 = dane3.readlines()
+
+for i in range(len(plik3)):
+    plik3[i] = plik3[i].strip()
+
+print(plik3)'''
+
+'''dane4 = open('plik4')
+plik4 = dane4.readlines()
+
+for i in range(len(plik4)):
+    plik4[i] = plik4[i].split()
+
+    print(plik4)'''
+dane5 = open('plik5')
+plik5 = dane5.readlines()
+
+for i in range(len(plik5)):
+     plik5[i] = plik5[i].split()
+     for j in range(len(plik5[i])):
+         plik5[i][j] = int(plik5[i][j])
+print(plik5)
+
+
+plik5prin = [list(map(int, x.split()))for x in open('plik5')]
